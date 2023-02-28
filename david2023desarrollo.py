e4bf45c56a92f260aec1c871f008cf9f5f4bf023
davidzabalaflow420 <125364873+davidzabalaflow420@users.noreply.github.com>
2023-02-28 07:39:40 -0500

    Add files via upload

---
+++ david2023desarrollo.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 06:56:05 2023
+
+@author: b12s309
+"""
+
+
+
+
+class plantas:
+    nombre = "Tienda de Juan" # Atributos
+    
+#Metodos
+
+    def __init__(self, nombre,edad,tipo,tiempovid):
+            self.nombre = nombre
+            self.edad = edad
+            self.tipo = tipo
+            self.tiempovid=tiempovid
+
+    def naranja(self):
+            self.nombre=  plantas("naranja")
+            self.edad= plantas ("8 meses")
+            self.tipo= plantas ("citrico")
+            self.tiempovid= plantas("8 años")
+    
+    def cebolla(self):
+            self.nombre=  plantas("cebolla")
+            self.edad= plantas ("3 meses")
+            self.tipo= plantas ("verdura")
+            self.tiempovid= plantas("2 años")    
+    
+    def lechuga(self):
+            self.nombre=  plantas("Lechuga")
+            self.edad= plantas ("23 meses")
+            self.tipo= plantas ("verdura")
+            self.tiempovid= plantas("9 años")    
+    
+    
+
+
+    
+
